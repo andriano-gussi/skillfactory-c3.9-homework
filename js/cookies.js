446108937a82fb5a8601cd38c5ace2eb1b517461
andriano-gussi <64980120+andriano-gussi@users.noreply.github.com>
2020-08-11 14:17:03 +0300

    Add files via upload

--- js/cookies.js
+++ js/cookies.js
@@ -1,9 +1,20 @@
+// для удобства записываем в переменные необходимые элементы страницы:
 const divCity = document.querySelector('#city');
 const inputCity = document.querySelector('input[name=city]');
 const divSavedCity = document.querySelector('#saved_city');
+const userCityName = document.querySelector('#user_city');
 const btnChangeCity = document.querySelector('#change_city');
+const h2 = document.querySelector('h2');
 const btnSaveChanges = document.querySelector('#save_changes');
+const boxes = document.querySelectorAll('.form-check-input');
 
+// присваеваем по умолчанию пустые значения для служебных переменных:
+// boxesStatus - для информации о состоянии чекбоксов
+// userCity - для хранения названия города пользователя
+let boxesStatus = '';
+let userCity = '';
+
+// записывает куки
 function setCookie(name, value, options = {}) {
   options = {
     path: '/',
@@ -26,6 +37,7 @@ function setCookie(name, value, options = {}) {
   document.cookie = updatedCookie;
 }
 
+// возвращает значение куки с указанным именем (name)
 function getCookie(name) {
   let matches = document.cookie.match(new RegExp(
     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
@@ -33,16 +45,70 @@ function getCookie(name) {
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
+// удаляет куку с именем name
 function deleteCookie(name) {
   setCookie(name, "", {
     'max-age': -1
   })
 }
 
+// устанавливае все чекбоксы в неактивное состояние
+function setBoxesDisabled(item) {
+  item.disabled = true;
+}
+
+// проверяет состояние каждого чекбокса и формирует строку boxesStatus:
+// в которую по каждому чекбоксу добавляется его состояние,
+// где "1" - когда чекбокс отмечен, и "0" - в противном случае 
+function saveBoxesStatus (item) {
+  item.checked ? boxesStatus = boxesStatus + '1' : boxesStatus = boxesStatus + '0';
+}
+
+// расшифровывает строку boxesStatus - в соответствии с ее содержимым
+// устанавливает для каждого чекбокса состояние: отмечен или не отмечен
+function setBoxesStatus () {
+  for (let i = 0; i < boxesStatus.length; i++) {
+    boxesStatus[i] == '1' ? boxes[i].checked = true : boxes[i].checked = false;
+  }
+}
+
+// обрабатываем клик по кнопке "Запомнить информацию"
 btnSaveChanges.addEventListener('click', () => {
-  console.log(inputCity.value);
-  document.cookie = "user=xxx; max-age=600";
-  document.cookie = 'userCity=' + inputCity.value + '; ' + 'max-age=600';
-  //setCookie('userCity', inputCity.value, {'max-age': 600});
-  //alert(document.cookie);
+  // записываем куку с названием города, который введен пользователем
+  setCookie('userCity', inputCity.value, {'max-age': 600});
+  
+  // готовим информцию о состоянии чекбоксов и записываем
+  // соответствующую куку
+  boxes.forEach(saveBoxesStatus);
+  setCookie('boxesStatus', boxesStatus, {'max-age': 600});
+  
+  alert('Все изменения сохранены! Можете перезагрузить страницу или браузер');
 });
+
+// обрабатываем клик по кнопке "Изменить"
+btnChangeCity.addEventListener('click', () => {
+  //удаляем ранее сохраненные куки, обновляем страницу
+  deleteCookie('userCity');
+  deleteCookie('boxesStatus');
+  location.reload();
+});
+
+// основная функция, читает куки, если они есть - отображает
+// на странице информацию с введенным ранее городом и выбранными ранее
+// чекбосками. Здесь активной будет только кнопка "Изменить",
+// при нажатии на которую можно будет изменить город и отметить чекбоксы
+function init() {
+  userCity = getCookie('userCity');
+  boxesStatus = getCookie('boxesStatus');
+  if (userCity) {
+    divCity.style.display = 'none';
+    h2.style.display = 'none';
+    btnSaveChanges.style.display = 'none';
+    userCityName.innerText = userCity;
+    setBoxesStatus();
+    boxes.forEach(setBoxesDisabled);
+    divSavedCity.style.display = 'flex';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', init);
